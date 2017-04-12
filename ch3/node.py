2258b0e40484b38dc89c173b1f072a4a0755faed
Jennifer <jennifer.gd42@gmail.com>
2017-04-12 11:38:30 -0700

    Create node class.

---
+++ ch3/node.py
@@ -0,0 +1,47 @@
+"""Implementation of a node.
+
+Useful for trees, linked lists, graphs, etc.
+"""
+
+
+class Node():
+    """A node for a linked list.
+
+    Make a node:
+
+        >>> apple = Node("apple")
+        >>> apple.data
+        'apple'
+        >>> print(apple.next)
+        None
+        >>> print(apple.prev)
+        None
+
+    And another:
+
+        >>> berry = Node("berry")
+        >>> apple.next = berry
+        >>> berry.prev = apple
+
+        >>> print(apple.next)
+        <Node berry>
+        >>> print(berry.prev)
+        <Node apple>
+        >>> print(berry.next)
+        None
+        
+    """
+
+    def __init__(self, data):
+        """Initialize the node's attributes."""
+
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+    def __repr__(self):
+        """A human-readable representation of a node."""
+
+        return f"<Node {self.data}>"
+
