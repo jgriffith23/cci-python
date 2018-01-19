60ec1a0af008b36e745e6d6584314aedb824ef71
Jennifer <jennifer.gd42@gmail.com>
2018-01-18 16:23:41 -0800

    Finish 1.2

--- ch1/cci_ch1.py
+++ ch1/cci_ch1.py
@@ -20,6 +20,7 @@ def is_unique(chars):
     - Keep a "seen" list, and compare its length with the string's length.
     - Use a dictionary to count; if a count goes higher than 1, return False.
     """
+
     for i in range(len(chars)):
         if chars[i] in chars[i + 1:]:
             return False
@@ -34,12 +35,24 @@ def is_permutation(string1, string2):
 
     >>> is_permutation('dog', 'ogd')
     True
+
+    >>> is_permutation("apple", "berry")
+    False
+
+    Runtime: O(m + n), where m is len(string1) and n is len(string2)
     """
 
-    print "FINISH THIS"
+    if len(string1) != len(string2):
+        return False
+
+    sorted_str1 = sorted(string1)
+    sorted_str2 = sorted(string2)
+
+    return sorted_str1 == sorted_str2
+
 
 if __name__ == "__main__":
     import doctest
 
     if doctest.testmod().failed == 0:
-        print "\n*** ALL TESTS PASSED. NICE CHALLENGE SOLUTIONS!***\n"
+        print("\n*** ALL TESTS PASSED. NICE CHALLENGE SOLUTIONS!***\n")
